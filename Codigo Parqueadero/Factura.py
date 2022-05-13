caf2318a75232c1af70e3d541793e2d68a067761
Mateo <mateon639@gmail.com>
2022-05-13 18:40:32 -0500

    Se modifico metodos factura

--- Codigo Parqueadero/Factura.py
+++ Codigo Parqueadero/Factura.py
@@ -1,8 +1,11 @@
-class Factura:
-	
-	def __init__(self):
-		self.Vehiculo = Vehiculo()
-		self.Usuario = Usuario()
-		self.Tiempo = Tiempo()
-		self.Tarifa = Tarifa()
-		self.MetodoDePago = MetodoDePago()
+class Factura(Vehiculo, ListaTransferenciasbancarias):
+	def __init__(self, Usuario, Tiempo, Tarifa, MetodoPago):
+		self.Usuario = Usuario
+		self.Tiempo = Tiempo
+		self.Tarifa = Tarifa
+		self.MetodoPago = MetodoPago
+		self.Vehiculo = Vehiculo
+
+    def ImprimirFactura(self, Tiempo, Tarifa):
+    	return self.Tarifa * self.Tiempo 
+    	print("Imprimir datos factura")
