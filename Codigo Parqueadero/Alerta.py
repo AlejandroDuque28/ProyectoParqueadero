37ecee5129c435829a76dd955b1ffa42d5db797e
Mateo <mateon639@gmail.com>
2022-05-26 13:42:46 -0500

    modificacion

--- Codigo Parqueadero/Alerta.py
+++ Codigo Parqueadero/Alerta.py
@@ -1,18 +1,25 @@
+import Vehiculo
+import Parqueadero
+import LugarVehiculo
+
 class Alerta:
-  def InformeSeguridad(string):
-    print("Alarma de vehiculo detectada") 
-    
-  def __init__(self, Vehiculo, Parqueadero, LugarVehiculo):
-    self.Vehiculo = Vehiculo
-    self.Parqueadero = Parqueadero
-    self.LugarVehiculo = LugarVehiculo
+    #Atributos
+    InformeSeguridad : str
 
-  def AlarmaCarro(self, InformeSeguridad, Vehiculo ):
-    print(InformeSeguridad)
-    print("Alarma encendida")
     
-  def ParqueaderoAbasto(self, Parqueadero):
-    print("El parqueadero esta abasto")
+    def __init__(self, Vehiculo:Vehiculo, Parqueadero:Parqueadero, LugarVehiculo:LugarVehiculo):
+      self.Vehiculo = Vehiculo
+      self.Parqueadero = Parqueadero
+      self.LugarVehiculo = LugarVehiculo
 
-  def ParqueaderoDisponible(self, Vehiculo):
-    print("El parqueadero esta disponible")
+
+    #Metodos
+    def AlarmaCarro(self):
+      print(InformeSeguridad)
+      print("Alarma encendida")
+    
+    def ParqueaderoAbasto(self):
+      print("El parqueadero esta abasto")
+
+    def ParqueaderoDisponible(self):
+      print("El parqueadero esta disponible")
