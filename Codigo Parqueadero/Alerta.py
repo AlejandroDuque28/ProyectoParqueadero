a5d47afd07ce0a507a71257763bac28f48da3968
Mateo <mateon639@gmail.com>
2022-05-26 13:52:24 -0500

    Se agrega clase

--- Codigo Parqueadero/Alerta.py
+++ Codigo Parqueadero/Alerta.py
@@ -3,7 +3,6 @@ import Parqueadero
 import LugarVehiculo
 
 class Alerta:
-<<<<<<< HEAD
     #Atributos
     InformeSeguridad : str
 
@@ -24,22 +23,4 @@ class Alerta:
 
     def ParqueaderoDisponible(self):
       print("El parqueadero esta disponible")
-=======
-  def InformeSeguridad(string):
-    print("Alarma de vehiculo detectada") 
-    
-  def __init__(self):
-    self.Vehiculo = Vehiculo
-    self.Parqueadero = Parqueadero
-    self.LugarVehiculo = LugarVehiculo
 
-  def AlarmaCarro(self):
-    print(InformeSeguridad)
-    print("Alarma encendida")
-    
-  def ParqueaderoAbasto(self):
-    print("El parqueadero esta abasto")
-
-  def ParqueaderoDisponible(self):
-    print("El parqueadero esta disponible")
->>>>>>> f5a7da26f3f470475aa38beb43f1217cfdd4ce83
