967386b53f47a9166780489fc94598bfdbc31869
AlejandroDuque28 <aduque94656@umanizales.edu.com>
2022-05-27 19:40:26 -0500

    Se agrega clase con las demas importadas y manejo de datos alejandro

---
+++ Codigo Parqueadero/ClaseMain.py
@@ -0,0 +1,19 @@
+import Administrador
+import Alerta
+import Cliente
+import Elementos
+import Factura
+import ListaTipoVehiculo
+import ListaTransferenciasbancarias
+import LugarElemento
+import LugarParqueadero
+import Mensualidades
+import Parqueadero
+import Reservas
+import Tarifa
+import TIcket
+import TicketReservas
+import Tiempo
+import Usuario
+import Vehiculo
+
---
+++ Codigo Parqueadero/ManejoDatosClasesAlejandro.py
@@ -0,0 +1,5 @@
+import pandas as pd 
+
+CLase1 = pd.read_csv('Usuario.csv', header=0)
+Clase2 = pd.read_csv('Administrador.csv',header=0)
+Clase3 = pd.read_cvs('Cliente.csv',header=0)
